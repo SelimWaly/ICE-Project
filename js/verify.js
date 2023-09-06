2b259bc35d38587bd6279e420b49446a3563e6f4
Selim Waly <selimwalyy@gmail.com>
2023-09-06 19:57:32 +0300

    Update verify.js

--- js/verify.js
+++ js/verify.js
@@ -5,14 +5,6 @@ document.addEventListener('keydown', function(event) {
     inputBuffer += event.key.toLowerCase();
     
     if (inputBuffer.includes(triggerPhrase)) {
-        if (inputBuffer === triggerPhrase) {
-            window.location.href = './verify.html';
-        }
-        else {
-            inputBuffer = '';
-        }
-
+        window.location.href = './verify.html';
     }
 });
-
-
