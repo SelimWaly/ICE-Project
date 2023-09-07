96be145adef911789c7da4e1533b29c5e850afc3
Selim Waly <selimwalyy@gmail.com>
2023-09-08 01:11:49 +0300

    Update and rename logintoadminpanel.js to login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,9 @@
 const correct_answer = "ADMIN";
 let useranswer = prompt("What Is The Passcode?");
-        if(useranswer == correct_answer){
+if (useranswer == correct_answer) {
         alert("Correct, Click Ok To Proceed")
-       }
-       else{
+}
+else {
         alert("Incorrect, Click Ok To Proceed")
         window.location.href = ("index.html");
-       }
+}
