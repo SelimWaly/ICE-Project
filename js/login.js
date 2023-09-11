deb4fa0d81d6bd23273fd4e2f6001446bd2b9423
Ahmed-M-Taha <132652114+Ahmed-M-Taha@users.noreply.github.com>
2023-09-10 21:08:14 -0700

    Changing Login.js to hide the adminpanel before entering the password
    Changing Login.js to hide the adminpanel before entering the password. if entered incorrectly it will redirect back and the body will remain hidden

--- js/login.js
+++ js/login.js
@@ -1,9 +1,12 @@
 const correct_answer = "Admin";
 let useranswer = prompt("What Is The Passcode?");
 if (useranswer == correct_answer) {
+    document.getElementsByTagName("body")[0].style.display = "none";
         alert("Correct, Click Ok To Proceed")
+        document.getElementsByTagName("body")[0].style.display = "block";
 }
 else {
+    document.getElementsByTagName("body")[0].style.display = "none";
         alert("Incorrect, Click Ok To Proceed")
         window.location.href = ("index.html");
 }
