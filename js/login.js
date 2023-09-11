b6ffeedea9f818f3ca52413deb957b82d612bc32
Selim Waly <selimwalyy@gmail.com>
2023-09-11 22:39:44 +0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,12 +1,11 @@
 const correct_answer = "Admin";
 let useranswer = prompt("What Is The Passcode?");
-if (useranswer == correct_answer) {
+
+if (useranswer === correct_answer) {
+    alert("Correct, Click Ok To Proceed");
     document.getElementsByTagName("body")[0].style.display = "none";
-        alert("Correct, Click Ok To Proceed")
-        document.getElementsByTagName("body")[0].style.display = "block";
-}
-else {
-    document.getElementsByTagName("body")[0].style.display = "none";
-        alert("Incorrect, Click Ok To Proceed")
-        window.location.href = ("index.html");
+    document.getElementsByTagName("body")[0].style.display = "block";
+} else {
+    alert("Incorrect, Click Ok To Proceed");
+    window.location.href = "index.html";
 }
